3a01c3f2603f55fcc18088f0fad75c9c7f2a3315
abhiabin <abhiabin6112@gmail.com>
2023-12-13 14:31:22 +0530

    add

---
+++ moviefolder/movieproject/movieapp/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+from movieapp.models import movie
+from.forms import MovieForm
+
+# Create your views here.
+def index(request):
+    obj=movie.objects.all()
+    return render(request,"index.html",{"result":obj})
+def detail(request,movie_id):
+    Movie=movie.objects.get(id=movie_id)
+    return render(request,"detail.html",{"movie":Movie})
+def add_movie(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        desc=request.POST.get('desc')
+        year=request.POST.get('year')
+        img=request.FILES['img']
+        Movie=movie(name=name,desc=desc,year=year,img=img)
+        Movie.save()
+
+    return render(request,"add.html")
+def update(request,id):
+    Movie=movie.objects.get(id=id)
+    form=MovieForm(request.POST or None,request.FILES,instance=Movie)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,"edit.html",{"form":form,"movie":Movie})
+def delete(request,id):
+    if request.method=='POST':
+        Movie=movie.objects.get(id=id)
+        Movie.delete()
+        return redirect('/')
+    return render(request,'delete.html')
